13f3b37e4481b6640c3c76b3cd96921d20837c47
Haofan Fang <haofanfang@gmail.com>
2018-07-31 11:36:55 +0800

    1.0

--- app/src/main/java/bjfu/bit/Test.java
+++ app/src/main/java/bjfu/bit/Test.java
@@ -3,6 +3,7 @@ package bjfu.bit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//我的设想是，先出现一张用来测试的图片，下面提示让你看图后语音说出指向，然后点击下一步按钮后，开始进行语音识别，然后重复n（按算法需要）次
 public class Test extends AppCompatActivity {
 
     @Override
